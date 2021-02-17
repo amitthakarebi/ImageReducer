b5ddc0194ecae21d59e5ea129fcde7eba771370e
amitthakarebi <amitthakarebi@users.noreply.github.com>
2021-02-17 23:43:57 +0530

    Update After Reduce with removing depricated insterstitial ads

--- app/src/main/java/com/amitthakare/imagereducer/AfterReduce.java
+++ app/src/main/java/com/amitthakare/imagereducer/AfterReduce.java
@@ -1,5 +1,6 @@
 package com.amitthakare.imagereducer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
@@ -12,10 +13,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
+//import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
@@ -27,7 +32,7 @@ public class AfterReduce extends AppCompatActivity {
 
     AdView bottomAd;
     ImageView imageView;
-    InterstitialAd interstitialAd;
+    InterstitialAd mInterstitialAd;
     Button finishedBtn;
 
     @Override
@@ -49,14 +54,16 @@ public class AfterReduce extends AppCompatActivity {
 
         adsIni();
 
+
+
         finishedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(AfterReduce.this, "Saved To Location!", Toast.LENGTH_SHORT).show();
-                if (interstitialAd.isLoaded()) {
-                    interstitialAd.show();
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(AfterReduce.this);
                 } else {
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    Log.e("TAG", "The interstitial ad wasn't ready yet.");
                 }
             }
         });
@@ -71,50 +78,52 @@ public class AfterReduce extends AppCompatActivity {
             }
         });
 
-
         bottomAd = findViewById(R.id.afterReducedBottomAds);
-
         AdRequest adRequest1 = new AdRequest.Builder().build();
         bottomAd.loadAd(adRequest1);
 
-        interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId("ca-app-pub-4970909843127323/8976400418");
-        interstitialAd.loadAd(new AdRequest.Builder().build());
+        AdRequest adRequest = new AdRequest.Builder().build();
 
-        interstitialAd.setAdListener(new AdListener() {
+        InterstitialAd.load(this,"ca-app-pub-4970909843127323/8976400418", adRequest, new InterstitialAdLoadCallback() {
             @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // The mInterstitialAd reference will be null until
+                // an ad is loaded.
+                mInterstitialAd = interstitialAd;
+                Log.i("Interstitial", "onAdLoaded");
+
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // Called when fullscreen content is dismissed.
+                        Log.e("TAG", "The ad was dismissed.");
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // Called when fullscreen content failed to show.
+                        Log.e("TAG", "The ad failed to show.");
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        // Make sure to set your reference to null so you don't
+                        // show it a second time.
+                        mInterstitialAd = null;
+                        Log.e("TAG", "The ad was shown.");
+                    }
+                });
+
             }
 
             @Override
-            public void onAdFailedToLoad(LoadAdError adError) {
-                // Code to be executed when an ad request fails.
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when the ad is displayed.
-            }
-
-            @Override
-            public void onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when the interstitial ad is closed.
-                finish();
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                // Handle the error
+                Log.i("Interstitial", loadAdError.getMessage());
+                mInterstitialAd = null;
             }
         });
-
-
 
 
     }
